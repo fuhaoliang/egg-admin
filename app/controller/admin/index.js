591d597f9c26ab18797f767b599a65b1ccfe762f
付豪亮 <fuhl@fosun.com>
2020-12-30 19:20:33 +0800

    egg-admin

---
+++ app/controller/admin/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class IndexController extends Controller {
+  async index() {
+    const { ctx } = this;
+    ctx.body = '';
+  }
+}
+
+
+module.exports = IndexController;
